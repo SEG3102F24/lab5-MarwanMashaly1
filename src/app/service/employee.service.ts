06125f2e426ce1206e9ecec5c42ccbaddcc7b230
Marwan <mmash102@uottawa.ca>
2024-10-20 16:01:59 -0400

    add firebase

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,19 +1,57 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject, Observable} from "rxjs";
-import {Employee} from "../model/employee";
+import { collectionData, Firestore } from '@angular/fire/firestore';
+import { addDoc, collection, DocumentReference } from '@firebase/firestore';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Employee } from '../model/employee';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeeService {
-  employees$: BehaviorSubject<readonly Employee[]> = new BehaviorSubject<readonly Employee[]>([]);
+  private employeesCollection = collection(this.firestore, 'employees');
+  employees$: BehaviorSubject<readonly Employee[]> = new BehaviorSubject<
+    readonly Employee[]
+  >([]);
+
+  constructor(private firestore: Firestore) {
+    this.getEmployees().subscribe({
+      next: (employees) => {
+        this.employees$.next(employees);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
 
   get $(): Observable<readonly Employee[]> {
     return this.employees$;
   }
 
-  addEmployee(employee: Employee) {
-    this.employees$.next([...this.employees$.getValue(), employee]);
-    return true;
+  getEmployees(): Observable<Employee[]> {
+    return collectionData(this.employeesCollection, { idField: 'id' }).pipe(
+      map((employees: any[]) => {
+        return employees.map((employee) => ({
+          ...employee,
+          dateOfBirth: new Date(employee.dateOfBirth),
+        }));
+      })
+    );
+  }
+
+  async addEmployee(employee: Employee): Promise<DocumentReference> {
+    try {
+      const data = {
+        ...employee,
+      };
+
+      const docRef = await addDoc(this.employeesCollection, data);
+      console.log('Document written with ID: ', docRef.id);
+      return docRef;
+    } catch (e) {
+      console.error('Error adding document: ', e);
+      throw e;
+    }
   }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  // Import the functions you need from the SDKs you need
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  firebase: {
+    apiKey: 'AIzaSyCuu9HvPHIZ5rpbvxv2gddYd0bChXdEf1M',
+    authDomain: 'seg3102-lab5-24cf5.firebaseapp.com',
+    projectId: 'seg3102-lab5-24cf5',
+    storageBucket: 'seg3102-lab5-24cf5.appspot.com',
+    messagingSenderId: '747086453541',
+    appId: '1:747086453541:web:041d185a196f03b0dad8cc',
+  },
+  // Initialize Firebase
+};
